20f12879583631d407c78c52aaa1169af0e53dc1
omkar2254 <omkarpatil2254@gmail.com>
2025-01-04 10:23:58 +0530

    passByValueOfReference

--- src/com.omkar/passByValueOfReference.java
+++ src/com.omkar/passByValueOfReference.java
@@ -1,11 +1,16 @@
 public class passByValueOfReference {
-  int value =10;
+  int value;
+  int newValue;
 
   static void primitiveMethod(int primitiveType){
     primitiveType = 30;
   }
   static void nonPrimitiveMethod(passByValueOfReference object){
     object.value = 40;
+  }
+  static void nonPrimitiveMethodReassinged(passByValueOfReference newObject){
+    newObject = new passByValueOfReference();
+    newObject.newValue = 80;
   }
   public static void main(String[] args){
     //Primitive data types
@@ -19,6 +24,12 @@ public class passByValueOfReference {
     System.out.println("Before modification: "+ object.value);
     nonPrimitiveMethod(object);
     System.out.println("After modification: "+ object.value);
+
+    passByValueOfReference newObject = new passByValueOfReference();
+    newObject.newValue = 70;
+    System.out.println("Before modification: "+ newObject.newValue);
+    nonPrimitiveMethodReassinged(newObject);
+    System.out.println("After modification: "+ newObject.newValue);
   }
 }
 
